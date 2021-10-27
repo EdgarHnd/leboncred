ed34315505c29529d7345855eb1c9b04c1a9c949
Edgar Haond <edgar.haond@gmail.com>
2021-10-27 23:42:06 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+/*
+* Gulpfile
+* @version: 1.0.0 (08 August, 2021)
+* @author: HtmlStream
+* @license: Htmlstream (https://htmlstream.com/licenses)
+* Copyright 2020 Htmlstream
+*/
+
+require('./gulpfiles/watch')
+require('./gulpfiles/dist')
+require('./gulpfiles/build')
